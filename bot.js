b0c3f63eed2e081223f5bf3c164135192bd48f64
Giovanni <giovanni_vidotto@hotmail.it>
2020-03-19 17:56:02 +0100

    Tolte le " " DC

--- bot.js
+++ bot.js
@@ -22,8 +22,8 @@ function login(username, chatId) {
         .then(res => {
             console.log(username);
             console.log(chatId);
-            const data = res.data;
-            let response = JSON.parse(data);
+            const data = res.data.toString();
+            let response = parse(data);
             axios.defaults.headers.common[Authorization] = 'Bearer '+response.token;
             let code = response.code;
             if (code === 1) {
@@ -35,13 +35,15 @@ function login(username, chatId) {
             }
         })
         .catch(err => {
+            // console.log("ERRORE")
+            // //console.log(err.status);
+            // if(true)
+            // {
+            //     mex = "Rieffettua l'autenticazione usando il comando /login";
+            // }else {
+            //     mex = 'Errore nel controllo dei dati';
+            // }
             console.log(err.response.status);
-            if(err.response.status === 403)
-            {
-                mex = "Rieffettua l'autenticazione usando il comando /login";
-            }else {
-                mex = 'Errore nel controllo dei dati';
-            }
 
         });
     return mex;
@@ -52,12 +54,15 @@ const server = http.createServer((req, res) => { //request and response object
         let jsonRes = '';
         req.on('data', data => {
             jsonRes += data.toString();
-            let response = JSON.parse(jsonRes);
+            console.log(parse(jsonRes));
+            let response = parse(jsonRes);
 
             let chatId = response.chat_id;
+            console.log(chatId);
             let authCode = response.auth_code;
+            console.log(authCode);
             axios
-                .post("https://api.telegram.org/bot"+tokenBot+"/sendMessage?chat_id="+chatId+"&text="+authCode)
+                .post(`https://api.telegram.org/bot${tokenBot}/sendMessage?chat_id=${chatId}&text=${authCode}`)
                 .then(res =>{console.log("Messaggio inviato con successo")})
                 .catch(err => {console.log("Errore nell'invio del messaggio")});
 
@@ -87,55 +92,55 @@ console.log('Server to port 3000');
 
 
     bot.command('login', message => {
-        //const url = message.message.text;
-        const username = message.from.username;
-        const chatId = message.from.id;
-        let mex = login(username, chatId);
-        return message.reply(mex);
+        // const url = message.message.text;
+        // const username = message.from.username;
+        // const chatId = message.from.id;
+        // let mex = login(username, chatId);
+        // return message.reply(mex);
     });
 
-     bot.command('status', message => {
-         const username = message.from.username;
+    //  bot.command('status', message => {
+    //      const username = message.from.username;
 
-         axios
-             .get(`http://localhost:9999/status/${username}`)
-             .then(res => {
-                 const data = res.data;
-                 let userInfo = JSON.parse(data);
+    //      axios
+    //          .get(`http://localhost:9999/status/${username}`)
+    //          .then(res => {
+    //              const data = res.data;
+    //              let userInfo = JSON.parse(data);
 
-                 let name = userInfo.name;
-                 let surname = userInfo.surname;
-                 let email = userInfo.email;
-                 let type = userInfo.type;
-                 let code = userInfo.code;
-                 let mex = "";
-                 if (code === 0) {
-                     mex = "Non hai attivato ancora l'autenticazione a due fattori nella web-app; usa /start non appena avrai fatto.";
-                 } else if (code === 1) {
-                     mex = "Hai attivato l'autenticazione a due fattori; usa /start per completare la procedura";
-                 } else if (code === 2) {
-                     mex = "L'autenticazione a due fattori è attiva";
-                 }
+    //              let name = userInfo.name;
+    //              let surname = userInfo.surname;
+    //              let email = userInfo.email;
+    //              let type = userInfo.type;
+    //              let code = userInfo.code;
+    //              let mex = "";
+    //              if (code === 0) {
+    //                  mex = "Non hai attivato ancora l'autenticazione a due fattori nella web-app; usa /start non appena avrai fatto.";
+    //              } else if (code === 1) {
+    //                  mex = "Hai attivato l'autenticazione a due fattori; usa /start per completare la procedura";
+    //              } else if (code === 2) {
+    //                  mex = "L'autenticazione a due fattori è attiva";
+    //              }
 
-                 return message.replyWithHTML('<ul>' +
-                     '<li>' + name + '</li>' +
-                     '<li>' + surname + '</li>' +
-                     '<li>' + email + '</li>' +
-                     '<li>' + type + '</li>' +
-                     '<li>' + mex + '</li>' +
-                     '</ul> ');
+    //              return message.replyWithHTML('<ul>' +
+    //                  '<li>' + name + '</li>' +
+    //                  '<li>' + surname + '</li>' +
+    //                  '<li>' + email + '</li>' +
+    //                  '<li>' + type + '</li>' +
+    //                  '<li>' + mex + '</li>' +
+    //                  '</ul> ');
 
 
-             })
-             .catch(err => {
-                 if(err.response.status === 403)
-                 {
-                     message.reply("Rieffettua l'autenticazione usando il comando /login");
-                 }else {
-                     message.reply('Errore nel controllo dei dati');
-                 }
-             });
-     });
+    //          })
+    //          .catch(err => {
+    //              if(err.response.status === 403)
+    //              {
+    //                  message.reply("Rieffettua l'autenticazione usando il comando /login");
+    //              }else {
+    //                  message.reply('Errore nel controllo dei dati');
+    //              }
+    //          });
+    //  });
 
 
     bot.command('info', ({ reply}) => reply(
@@ -144,21 +149,21 @@ console.log('Server to port 3000');
     ));
 
 
-    bot.command('test', (message,token,chatID) => {
-        token = 123456;
-        chatID = 226026286;
-        axios
-        .post(`https://api.telegram.org/bot${tokenBot}/sendMessage?chat_id=${chatID}&text=${token}`)
-        //https://api.telegram.org/bot1120382460:AAG2TTBT-GqHcIfGzH_TYOvCZFI0pMEu88c/sendMessage?chat_id="226026285"&text="ciao"
-        .then(res => {
-            console.log(res);
-            return message.reply('Token inviato');
-        })
-        .catch(err => {
-            console.log(err.response.status);
-            return message.reply(`Errore nell'invio del messaggio`);
-        });
-    });
+    // bot.command('test', (message,token,chatID) => {
+    //     token = 123456;
+    //     chatID = 226026285;
+    //     axios
+    //     .post(`https://api.telegram.org/bot${tokenBot}/sendMessage?chat_id=${chatID}&text=${token}`)
+    //     //https://api.telegram.org/bot1120382460:AAG2TTBT-GqHcIfGzH_TYOvCZFI0pMEu88c/sendMessage?chat_id="226026285"&text="ciao"
+    //     .then(res => {
+    //         console.log(res);
+    //         return message.reply('Token inviato');
+    //     })
+    //     .catch(err => {
+    //         console.log(err.response.status);
+    //         return message.reply(`Errore nell'invio del messaggio`);
+    //     });
+    // });
     console.log('Bot avviato correttamente');
     bot.launch();
 
