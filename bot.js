c6f1e199fda5c6ad0b05cf0957a007ffd8224350
Alessandro <tommasinalessandro@gmail.com>
2020-03-27 10:32:56 +0100

    Spostato server

--- bot.js
+++ bot.js
@@ -6,43 +6,9 @@ const axios = require("axios");
 
 // tokenbot e creazione bot
 const tokenBot = process.env.BOT_TOKEN;
-console.log(tokenBot);
-
 const bot = new Telegraf(tokenBot);
-// Richiesta per creazione server
-const http = require("http");
-// const { parse } = require("querystring");
 
-const server = http.createServer((req, res) => {
-  // Request and Response object
-  if (req.method === "POST") {
-    let jsonRes = "";
-    req.on("data", (data) => {
-      jsonRes += data.toString();
-      console.log(JSON.parse(jsonRes));
-      const response = JSON.parse(jsonRes);
-      const chatId = response.chat_id;
-      const authCode = response.auth_code;
-      axios
-        .post(
-          `https://api.telegram.org/bot${tokenBot}/sendMessage?chat_id=${chatId}&text=Ecco il tuo codice di autenticazione: ${authCode}`
-        )
-        .then(() => {
-          console.log("Messaggio inviato con successo");
-        })
-        .catch((err) => {
-          console.log(
-            "Errore " + err.response.status + " nell'invio del messaggio"
-          );
-        });
-    });
-    req.on("end", () => {
-      console.log(JSON.parse(jsonRes));
-      res.end("ok");
-    });
-    console.log(jsonRes);
-  }
-});
+const server = require("./botServer");
 
 server.listen(3000, "127.0.0.1");
 console.log("Server to port 3000");
---
+++ server.js
@@ -0,0 +1,33 @@
+// Richiesta per creazione server
+const http = require("http");
+
+exports.botServer = http.createServer((req, res) => {
+  // Request and Response object
+  if (req.method === "POST") {
+    let jsonRes = "";
+    req.on("data", (data) => {
+      jsonRes += data.toString();
+      console.log(JSON.parse(jsonRes));
+      const response = JSON.parse(jsonRes);
+      const chatId = response.chat_id;
+      const authCode = response.auth_code;
+      axios
+        .post(
+          `https://api.telegram.org/bot${tokenBot}/sendMessage?chat_id=${chatId}&text=Ecco il tuo codice di autenticazione: ${authCode}`
+        )
+        .then(() => {
+          console.log("Messaggio inviato con successo");
+        })
+        .catch((err) => {
+          console.log(
+            "Errore " + err.response.status + " nell'invio del messaggio"
+          );
+        });
+    });
+    req.on("end", () => {
+      console.log(JSON.parse(jsonRes));
+      res.end("ok");
+    });
+    console.log(jsonRes);
+  }
+});
