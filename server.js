703b838af111549fc2ac2a161894f65a7c76365f
Alessandro <tommasinalessandro@gmail.com>
2020-03-28 12:42:52 +0100

    Update richieste server

--- server.js
+++ server.js
@@ -12,19 +12,44 @@ exports.botServer = http.createServer((req, res) => {
       console.log(JSON.parse(jsonRes));
       const response = JSON.parse(jsonRes);
       const chatId = response.chat_id;
-      const authCode = response.auth_code;
-      axios
-        .post(
-          `https://api.telegram.org/bot${tokenBot}/sendMessage?chat_id=${chatId}&text=Ecco il tuo codice di autenticazione: ${authCode}`
-        )
-        .then(() => {
-          console.log("Messaggio inviato con successo");
-        })
-        .catch((err) => {
-          console.log(
-            "Errore " + err.response.status + " nell'invio del messaggio"
-          );
-        });
+      if (response.req_type == "authentication") {
+        const authCode = response.auth_code;
+        axios
+          .post(
+            `https://api.telegram.org/bot${tokenBot}/sendMessage?chat_id=${chatId}&text=Ecco il tuo codice di autenticazione: ${authCode}`
+          )
+          .then(() => {
+            console.log("Messaggio inviato con successo");
+          })
+          .catch((err) => {
+            console.log(
+              "Errore " + err.response.status + " nell'invio del messaggio"
+            );
+          });
+      } else if (response.req_type == "alert") {
+        const deviceId = response.device_id;
+        const sensorId = response.sensor_id;
+        const sensorValue = response.sensor_value;
+        const threshold = response.threshold;
+        const valueType = response.value_type;
+
+        const message1 = `⚠️ Il sensore ${sensorId} del dispositivo ${deviceId} ha registrato un valore di `;
+        const message2 = `${sensorValue} ${valueType} superando la soglia di ${threshold}`;
+        axios
+          .post(
+            `https://api.telegram.org/bot${tokenBot}/sendMessage?chat_id=${chatId}&text=` +
+              message1 +
+              message2
+          )
+          .then(() => {
+            console.log("Messaggio inviato con successo");
+          })
+          .catch((err) => {
+            console.log(
+              "Errore " + err.response.status + " nell'invio del messaggio"
+            );
+          });
+      }
     });
     req.on("end", () => {
       console.log(JSON.parse(jsonRes));
