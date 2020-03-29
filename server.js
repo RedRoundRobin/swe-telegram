9a51a399bbec49daae686350e8e1897b868a8e83
Alessandro <tommasinalessandro@gmail.com>
2020-03-29 12:57:19 +0200

    Aggiunta possibilità di ricevere più chatId  per alert

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ exports.botServer = http.createServer((req, res) => {
       jsonRes += data.toString();
       console.log(JSON.parse(jsonRes));
       const response = JSON.parse(jsonRes);
-      const chatId = response.chat_id;
-      if (response.req_type == "authentication") {
-        const authCode = response.auth_code;
+      if (response.reqType == "authentication") {
+        const authCode = response.authCode;
+        const chatId = response.chat_id;
         axios
           .post(
             `https://api.telegram.org/bot${tokenBot}/sendMessage?chat_id=${chatId}&text=Ecco il tuo codice di autenticazione: ${authCode}`
@@ -26,7 +26,8 @@ exports.botServer = http.createServer((req, res) => {
               "Errore " + err.response.status + " nell'invio del messaggio"
             );
           });
-      } else if (response.req_type == "alert") {
+      } else if (response.reqType == "alert") {
+        const chatsId = response.chat_id;
         const deviceId = response.device_id;
         const sensorId = response.sensor_id;
         const sensorValue = response.sensor_value;
@@ -34,22 +35,26 @@ exports.botServer = http.createServer((req, res) => {
         const valueType = response.value_type;
         const message1 = `Attenzione: il sensore ${sensorId} del dispositivo ${deviceId} ha registrato un valore di `;
         const message2 = `${sensorValue} ${valueType} superando la soglia (${threshold})`;
-        axios
-          .post(
-            `https://api.telegram.org/bot${tokenBot}/sendMessage?chat_id=${chatId}&text=` +
-              message1 +
-              message2
-          )
-          .then(() => {
-            console.log("Messaggio inviato con successo");
-          })
-          .catch((err) => {
-            console.log("Errore: " + err + " nell'invio del messaggio");
-          });
+        // eslint-disable-next-line guard-for-in
+        for (const index in chatsId) {
+          console.log(chatsId[index]);
+          const chatId = chatsId[index];
+          axios
+            .post(
+              `https://api.telegram.org/bot${tokenBot}/sendMessage?chat_id=${chatId}&text=` +
+                message1 +
+                message2
+            )
+            .then(() => {
+              console.log("Messaggio inviato con successo");
+            })
+            .catch((err) => {
+              console.log("Errore: " + err + " nell'invio del messaggio");
+            });
+        }
       }
     });
     req.on("end", () => {
-      console.log(JSON.parse(jsonRes));
       res.end("ok");
     });
   }
