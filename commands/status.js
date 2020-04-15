22e677c107cfcddf069d370103712b6aa669266f
Fritz <nicolo.frison.1@studenti.unipd.it>
2020-04-15 22:53:46 +0200

    Fix telegram /status che fa richiesta a /users?telegramName=username

--- commands/status.js
+++ commands/status.js
@@ -2,23 +2,26 @@
 const axios = require("axios");
 
 const botStatus = (bot) => {
-    bot.command("status", (message) => {
-        axios
-            .get(`http://thirema-api:9999/status`)
-            .then((res) => {
-                const data = res.data;
-                const name = data.name;
-                const surname = data.surname;
-                const email = data.email;
-                const typeNumber = data.type;
-                let type = "Utente";
-                if (typeNumber === 1) {
-                    type = "Moderatore";
-                } else if (typeNumber === 2) {
-                    type = "Amministratore";
-                }
-                return message.replyWithMarkdown(
-                    `
+  bot.command("status", (message) => {
+    const username = message.from.username;
+    console.log(username);
+    axios
+      .get(`http://localhost:9999/users?telegramName=` + username)
+      .then((res) => {
+        console.log(res.data);
+        const data = res.data[0];
+        const name = data.name;
+        const surname = data.surname;
+        const email = data.email;
+        const typeNumber = data.type;
+        let type = "Utente";
+        if (typeNumber === 1) {
+          type = "Moderatore";
+        } else if (typeNumber === 2) {
+          type = "Amministratore";
+        }
+        return message.replyWithMarkdown(
+          `
     Ecco i tuoi dati *${message.from.username}*
     - *Nome:* ${name}
     - *Cognome:* ${surname}
