aaaf999a0c43faf4ce104df356d66ea152b212c2
Giovanni <giovanni_vidotto@hotmail.it>
2020-04-23 19:11:50 +0200

    Caricamento comandi dinamico

--- commands/info.js
+++ commands/info.js
@@ -1,4 +1,3 @@
-
 const botInfo = (bot) => {
   bot.command("info", ({ replyWithMarkdown }) => {
     replyWithMarkdown(`
--- main.js
+++ main.js
@@ -5,6 +5,25 @@ const Telegraf = require("telegraf");
 const tokenBot = process.env.BOT_TOKEN;
 const bot = new Telegraf(tokenBot);
 
+// Richiesta POST per settare i comandi
+const axios = require("axios");
+const botCommands = [
+  { command: "info", description: "Comandi supportati" },
+  { command: "login", description: "Autenticazione al sistema ThiReMa" },
+  { command: "status", description: "Controllo dello status dell'utente" },
+  { command: "dispositivi", description: "Visualizza lista dispositivi" },
+];
+axios
+  .post(`https://api.telegram.org/bot${tokenBot}/setMyCommands`, {
+    commands: botCommands,
+  })
+  .then(() => {
+    console.log("Comandi caricati");
+  })
+  .catch(() => {
+    console.log("Errore caricamento comandi");
+  });
+
 // eslint-disable-next-line no-unused-vars
 const { botServer } = require("./server");
 const botLaunch = require("./commands/launch");
