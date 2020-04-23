c0d7187f24cedc2c92f75f2056210124f25155c6
Alessandro <tommasinalessandro@gmail.com>
2020-04-23 17:16:46 +0200

    fix

--- commands/info.js
+++ commands/info.js
@@ -1,25 +1,25 @@
 const botInfo = (bot) => {
-  bot.command("info", ( message, { replyWithMarkdown } ) =>
-      const chatId=message.chat.id;
+  bot.command("info", ({ replyWithMarkdown }) => {
     replyWithMarkdown(`
 Ecco la lista dei comandi disponibili:
 - Login: /login
 - Status: /status
 - Info: /info
 sssss
-  `)
-  // let options = {
-  //   reply_markup: JSON.stringify({
-  //     inline_keyboard: [
-  //       [{ text: 'OK 1', callback_data: '1' }],
-  //       [{ text: 'OK 2', callback_data: '2' }],
-  //       [{ text: 'OK 3', callback_data: '3' }]
-  //     ]
-  //   })
-  // };
-  // bot.sendMessage('Provaaaaa', chatId)
-  //     .then( () => {
-  //       console.log("OK")
-  // });
+  `);
+    // let options = {
+    //   reply_markup: JSON.stringify({
+    //     inline_keyboard: [
+    //       [{ text: 'OK 1', callback_data: '1' }],
+    //       [{ text: 'OK 2', callback_data: '2' }],
+    //       [{ text: 'OK 3', callback_data: '3' }]
+    //     ]
+    //   })
+    // };
+    // bot.sendMessage('Provaaaaa', chatId)
+    //     .then( () => {
+    //       console.log("OK")
+    // });
+  });
 };
 module.exports.botInfo = botInfo;
