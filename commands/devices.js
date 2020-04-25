560d27cec691b4b0b6a5d6b2068321ee2a75476a
Giovanni <giovanni_vidotto@hotmail.it>
2020-04-25 18:23:56 +0200

    Update devices.js

--- commands/devices.js
+++ commands/devices.js
@@ -2,8 +2,32 @@ require("dotenv").config();
 const axios = require("axios");
 let admin = false;
 
+const callBackFunction = (message,bot) => {
+  console.log(message.chat.id);
+  const options = {
+    reply_markup: JSON.stringify({
+      keyboard: [
+        [{ text: "Attiva sensore", callback_data: "1" }],
+        [{ text: "Disattiva sensore", callback_data: "2" }],
+      ],
+    }),
+  };
+  console.log(options);
+  bot.telegram
+    .sendMessage(message.chat.id, "Segli l'opzione", options)
+    .then(function (sended) {
+      // `sended` is the sent message.
+      console.log("FATTO");
+    })
+    .catch(() => {
+      console.log("Errore");
+    });
+};
+
 const botDevices = (bot) => {
+  let message2;
   bot.command("devices", (message) => {
+    message2=message;
     const username = message.from.username;
     const getType = async () => {
       const resType = await axios
@@ -13,25 +37,23 @@ const botDevices = (bot) => {
           const typeNumber = data.type;
           if (typeNumber === 2) {
             admin = true;
-            console.log("IS ADMIN")
+            console.log("IS ADMIN");
           } else {
             admin = false;
-            return message.reply("Devi essere amministratore per vedere la lista dispositivi")
+            return message.reply(
+              "Devi essere amministratore per vedere la lista dispositivi"
+            );
           }
         })
         .catch((err) => {
-          if (err.response.status === 403) {
-            admin = false;
-            return message.reply("Devi essere amministratore per vedere la lista dispositivi")
-          } else {
-            admin = false;
-            return message.reply(`Esegui di nuovo il comando /login`);
-          }
+          admin = false;
+          return message.reply(`Esegui di nuovo il comando /login`);
         });
       const typeUser = await resType;
       return typeUser;
     };
     getType().then(() => {
+      console.log(getType());
       if (admin) {
         const buttonList = [];
         const getButtons = async () => {
@@ -41,10 +63,12 @@ const botDevices = (bot) => {
               admin = true;
               const devices = res.data;
               devices.forEach((device) => {
-                buttonList.push([{ text: device.name, callback_data: "1" }]);
+                buttonList.push([
+                  { text: device.name, callback_data: "callBackFunction(message2,bot)" },
+                ]);
               });
             })
-            .catch((err) => {
+            .catch(() => {
               admin = false;
             });
           const buttonsData = await response;
@@ -53,7 +77,7 @@ const botDevices = (bot) => {
         getButtons().then(() => {
           const options = {
             reply_markup: JSON.stringify({
-              inline_keyboard: buttonList,
+              keyboard: buttonList,
             }),
           };
           bot.telegram
