888c6ccef123a90bddf33eda84d84f05d5a5d457
Mariano <marianosciacco@gmail.com>
2020-04-25 20:34:23 +0200

    Update bot.test.js

--- __tests__/bot.test.js
+++ __tests__/bot.test.js
@@ -1,8 +1,8 @@
 // test bot
 const { botStart } = require("../commands/start");
-const { botInfo } = require("../commands/help");
+const { botInfo } = require("../commands/info");
 const { botLogin } = require("../commands/login");
-const { botStatus } = require("../commands/info");
+const { botHelp } = require("../commands/help");
 
 const Telegraf = require("telegraf");
 const tokenBot = process.env.BOT_TOKEN;
@@ -13,16 +13,16 @@ test("Check login", () => {
   expect(botLogin(bot)).toBe(undefined);
 });
 // LAUNCH
-test("Check status", () => {
-  expect(botStatus(bot)).toBe(undefined);
+test("Check info", () => {
+  expect(botInfo(bot)).toBe(undefined);
 });
 // START
 test("Check status", () => {
   expect(botStart(bot)).toBe(undefined);
 });
 // INFO
-test("Check info", () => {
-  expect(botInfo(bot)).toBe(undefined);
+test("Check help", () => {
+  expect(botHelp(bot)).toBe(undefined);
 });
 // const s = {"botLaunch": [Function botLaunch]`;
 // const t = JSON.parse(s);
