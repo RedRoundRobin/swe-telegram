802b08607bc32407632e87607b1eccd058e3d58f
Giovanni <giovanni_vidotto@hotmail.it>
2020-04-25 16:21:04 +0200

    Admin

--- commands/devices.js
+++ commands/devices.js
@@ -1,35 +1,70 @@
 require("dotenv").config();
 const axios = require("axios");
+let admin = false;
 
 const botDevices = (bot) => {
   bot.command("devices", (message) => {
-    const buttonList = [];
-    const getButtons = async () => {
-      const response = await axios
-        .get(`${process.env.URL_API}/devices`)
+    const username = message.from.username;
+    const getType = async () => {
+      const resType = await axios
+        .get(`${process.env.URL_API}/users?telegramName=${username}`)
         .then((res) => {
-          const devices = res.data;
-          devices.forEach((device) => {
-            buttonList.push([{ text: device.name, callback_data: "1" }]);
-          });
+          const data = res.data[0];
+          const typeNumber = data.type;
+          if (typeNumber === 2) {
+            admin = true;
+            console.log("IS admin");
+          }
+          else {
+            admin = false;
+            console.log("IS not admin");
+          }
         })
         .catch(() => {
-          return reply.message("Errore caricamento lista dispositivi");
+          return message.reply(`Errore verifica account, esegui il comando /login`);
         });
-      const buttonsData = await response;
-      return buttonsData;
+      const typeUser = await resType;
+      return typeUser;
     };
-    getButtons().then(() => {
-      const options = {
-        reply_markup: JSON.stringify({
-          inline_keyboard: buttonList,
-        }),
-      };
-      bot.telegram
-        .sendMessage(message.chat.id, "Ecco la lista dei dispositivi:", options)
-        .then(function (sended) {
-          // `sended` is the sent message.
+    getType().then(() => {
+      if (admin) {
+        const buttonList = [];
+        const getButtons = async () => {
+          const response = await axios
+            .get(`${process.env.URL_API}/devices`)
+            .then((res) => {
+              admin = true;
+              const devices = res.data;
+              devices.forEach((device) => {
+                buttonList.push([{ text: device.name, callback_data: "1" }]);
+              });
+            })
+            .catch((err) => {
+              admin = false;
+              return message.reply("Devi essere un amministratore");
+            });
+          const buttonsData = await response;
+          return buttonsData;
+        };
+        getButtons().then(() => {
+          const options = {
+            reply_markup: JSON.stringify({
+              inline_keyboard: buttonList,
+            }),
+          };
+          bot.telegram
+            .sendMessage(
+              message.chat.id,
+              "Ecco la lista dei dispositivi",
+              options
+            )
+            .then(function (sended) {
+              // `sended` is the sent message.
+            });
         });
+      } else {
+        return message.reply("Devi essere un amministratore");
+      }
     });
   });
 };
